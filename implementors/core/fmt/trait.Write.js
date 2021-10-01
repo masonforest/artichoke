38831404e252a642b6cb6c139ba7b9401a2ebd9f
artichoke-ci <ci@artichokeruby.org>
2021-10-01 23:16:27 +0000

    deploy: ea7093922ce7951f744d2e2a90189bc9b8b8a371

---
+++ implementors/core/fmt/trait.Write.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["spinoso_string"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"spinoso_string/struct.String.html\" title=\"struct spinoso_string::String\">String</a>","synthetic":false,"types":["spinoso_string::String"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
